c78cd4ebd0307ce68ac9a06a9efcd87e9bb14d30
bdcohen1 <bdcohen@iastate.edu>
2019-03-03 03:47:41 -0600

    nutrient info

--- app/src/main/java/com/example/pickhacks2019/changeText.java
+++ app/src/main/java/com/example/pickhacks2019/changeText.java
@@ -3,12 +3,24 @@ package com.example.pickhacks2019;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+
+import com.fatsecret.platform.model.CompactFood;
+import com.fatsecret.platform.model.CompactRecipe;
+import com.fatsecret.platform.model.Food;
+import com.fatsecret.platform.model.Recipe;
+import com.fatsecret.platform.services.FatsecretService;
+import com.fatsecret.platform.services.Response;
+import com.fatsecret.platform.services.android.Request;
+import com.fatsecret.platform.services.android.ResponseListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class changeText extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +41,39 @@ public class changeText extends AppCompatActivity {
         button3.setText(input.get(2));
         button4.setText(input.get(3));
         button5.setText(input.get(4));
+
+        button1.setOnClickListener(this::onClick1);
+        button2.setOnClickListener(this::onClick2);
+        button3.setOnClickListener(this::onClick3);
+        button4.setOnClickListener(this::onClick4);
+        button5.setOnClickListener(this::onClick5);
     }
+
+    protected void onClick1(View view){
+        Button button1 = findViewById(R.id.button1);
+        showNutrients((String)button1.getText());
+    }
+    protected void onClick2(View view){
+        Button button1 = findViewById(R.id.button2);
+        showNutrients((String)button1.getText());
+    }
+    protected void onClick3(View view){
+        Button button1 = findViewById(R.id.button3);
+        showNutrients((String)button1.getText());
+    }
+    protected void onClick4(View view){
+        Button button1 = findViewById(R.id.button4);
+        showNutrients((String)button1.getText());
+    }
+    protected void onClick5(View view){
+        Button button1 = findViewById(R.id.button5);
+        showNutrients((String)button1.getText());
+    }
+
+    protected void showNutrients(String str){
+        Intent intent = new Intent(this, nutrientInfo.class);
+        intent.putExtra(EXTRA_MESSAGE, str);
+        startActivity(intent);
+    }
+
 }
